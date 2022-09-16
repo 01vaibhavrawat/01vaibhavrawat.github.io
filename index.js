b8c9d71d566ed975cc5d955a9bdf8468acba3567
Vaibhav Rawat <vaibhrawat@gmail.com>
2022-09-16 10:07:17 +0530

    almost there

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 document.getElementById("filepicker").addEventListener("change", (event) => {
-  let output = document.getElementById("listing");
+  let table = document.getElementById("table");
   for (const file of event.target.files) {
-    let item = document.createElement("li");
-    item.textContent = file.webkitRelativePath;
-    output.appendChild(item);
+    let tr = document.createElement("tr");
+    let td = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    td.textContent = file.name.replace(/\.[^/.]+$/, "");
+    td2.textContent = file.size;
+    td3.textContent = "info";
+    tr.append(td, td2, td3);
+    table.appendChild(tr);
   };
 }, false);
